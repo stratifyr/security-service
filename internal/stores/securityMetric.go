6ca8299c4cf546cfaa1865fb4b57e076e6da86ca
Abishek R V <115567230+abishek-r-v@users.noreply.github.com>
2026-08-30 00:32:46 +0530

    Move security metrics cache logic to store layer (#28)
    * Add SecurityMetricStore for security-metrics cache
    * Move security-metrics cache to store layer

---
+++ internal/stores/securityMetric.go
@@ -0,0 +1,109 @@
+package stores
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/vmihailenco/msgpack/v5"
+	"gofr.dev/pkg/gofr"
+)
+
+const (
+	SecurityMetricCacheKey     = "security_metrics:security_id:%d:date:%s"
+	SecurityMetricCacheTimeout = 5 * time.Hour
+)
+
+type SecurityMetricStore interface {
+	Index(ctx *gofr.Context, securityIDs []int, date time.Time) ([]*SecurityMetric, error)
+	Create(ctx *gofr.Context, securityMetrics []*SecurityMetric, date time.Time) error
+}
+
+type SecurityMetric struct {
+	SecurityID int
+	MetricID   int
+	Date       time.Time
+	Value      float64
+	ZValue     float64
+}
+
+type securityMetricCacheValue struct {
+	MetricID int
+	Value    float64
+	ZValue   float64
+}
+
+type securityMetricStore struct{}
+
+func NewSecurityMetricStore() *securityMetricStore {
+	return &securityMetricStore{}
+}
+
+func (s *securityMetricStore) Index(ctx *gofr.Context, securityIDs []int, date time.Time) ([]*SecurityMetric, error) {
+	keys := make([]string, len(securityIDs))
+
+	for i, securityID := range securityIDs {
+		keys[i] = fmt.Sprintf(SecurityMetricCacheKey, securityID, date.Format(time.DateOnly))
+	}
+
+	vals, err := ctx.Redis.MGet(ctx, keys...).Result()
+	if err != nil {
+		return nil, err
+	}
+
+	var securityMetrics []*SecurityMetric
+
+	for i, val := range vals {
+		if val == nil {
+			ctx.Logger.Warnf("cache miss: %s", keys[i])
+			continue
+		}
+
+		var metricVals []*securityMetricCacheValue
+		if err = msgpack.Unmarshal([]byte(val.(string)), &metricVals); err != nil {
+			return nil, err
+		}
+
+		for _, mv := range metricVals {
+			securityMetrics = append(securityMetrics, &SecurityMetric{
+				SecurityID: securityIDs[i],
+				MetricID:   mv.MetricID,
+				Date:       date,
+				Value:      mv.Value,
+				ZValue:     mv.ZValue,
+			})
+		}
+	}
+
+	return securityMetrics, nil
+}
+
+func (s *securityMetricStore) Create(ctx *gofr.Context, securityMetrics []*SecurityMetric, date time.Time) error {
+	grouped := make(map[int][]*securityMetricCacheValue)
+
+	for _, metric := range securityMetrics {
+		grouped[metric.SecurityID] = append(grouped[metric.SecurityID], &securityMetricCacheValue{
+			MetricID: metric.MetricID,
+			Value:    metric.Value,
+			ZValue:   metric.ZValue,
+		})
+	}
+
+	pipe := ctx.Redis.Pipeline()
+
+	for securityID, metrics := range grouped {
+		key := fmt.Sprintf(SecurityMetricCacheKey, securityID, date.Format(time.DateOnly))
+
+		bytes, err := msgpack.Marshal(metrics)
+		if err != nil {
+			return err
+		}
+
+		pipe.SetEx(ctx, key, bytes, SecurityMetricCacheTimeout)
+	}
+
+	if _, err := pipe.Exec(ctx); err != nil {
+		return err
+	}
+
+	return nil
+}
